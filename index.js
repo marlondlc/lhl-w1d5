072b1f21bdde7e617fd8668a5b1b6985805cd222
Marlon <m.de.la.cruz@hotmail.com>
2019-03-08 17:39:10 +0000

    changed the chalk file

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var chalk = require("chalk");
+const log = console.log;
 
 var message = "Hello " + chalk.yellow("World");
-console.log(message);
+log(message)
+
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
